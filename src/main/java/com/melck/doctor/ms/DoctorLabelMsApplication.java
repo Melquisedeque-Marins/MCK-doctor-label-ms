57862a3b913f112fd4c601345b40240a5f97c3a6
Melquisedeque <melck_junior@hotmail.com>
2022-12-27 19:22:11 +0000

    Update application.properties to config eureka client

--- src/main/java/com/melck/doctor/ms/DoctorLabelMsApplication.java
+++ src/main/java/com/melck/doctor/ms/DoctorLabelMsApplication.java
@@ -5,11 +5,13 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @EnableFeignClients
 @EnableCaching
 @SpringBootApplication
+@EnableEurekaClient
 public class DoctorLabelMsApplication {
 
 	private static Logger logger = LoggerFactory.getLogger(DoctorLabelMsApplication.class);
