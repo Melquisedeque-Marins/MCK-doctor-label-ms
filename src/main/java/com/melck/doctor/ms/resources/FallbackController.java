36a993586130d2eb6912f312addab37cf49c312c
Melquisedeque <melck_junior@hotmail.com>
2022-12-30 21:45:55 +0000

    FallbackController

--- src/main/java/com/melck/doctor/ms/resources/FallbackController.java
+++ src/main/java/com/melck/doctor/ms/resources/FallbackController.java
@@ -1,5 +1,25 @@
 package com.melck.doctor.ms.resources;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/fallback")
 public class FallbackController {
+
+    @GetMapping("/doctorLabel")
+    public ResponseEntity<String> doctorLabelFallback () {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+            .body("it is a internam proccessament problem in order API. try again later");
+    }
+
+    @GetMapping("/label")
+    public ResponseEntity<String> labelFallback () {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+            .body("it is a internam proccessament problem in payment API. try again later");
+    }
     
 }
